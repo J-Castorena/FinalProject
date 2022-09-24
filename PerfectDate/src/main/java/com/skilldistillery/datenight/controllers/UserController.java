5dd91218d0fac94682d9eb72634d836c36a9c531
diegoescutia <escutia.diego@gmail.com>
2022-09-24 18:31:42 -0400

    User complete, address not fully functional

--- PerfectDate/src/main/java/com/skilldistillery/datenight/controllers/UserController.java
+++ PerfectDate/src/main/java/com/skilldistillery/datenight/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.datenight.controllers;
 
+import java.security.Principal;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
@@ -20,90 +21,58 @@ import com.skilldistillery.datenight.services.UserService;
 
 @CrossOrigin({ "*", "http://localhost/" })
 @RestController
-@RequestMapping(path= "api")
+@RequestMapping(path = "api")
 public class UserController {
-	
+
 	@Autowired
 	private UserService userSvc;
-	
+
 	@GetMapping("users")
-	public List<User> index(){
+	public List<User> index(Principal principal) {
 		return userSvc.index();
 	}
-	
-	@GetMapping("users/{userId}")
-	public User getById(@PathVariable int userId, HttpServletResponse res ) {
-		User user = userSvc.getUserById(userId);
-		if(user == null) {
+
+	@GetMapping("users/uid/{userId}")
+	public User getById(@PathVariable int userId, HttpServletResponse res, Principal principal) {
+		User user = userSvc.getUserById(userId, principal.getName());
+		if (user == null) {
 			res.setStatus(404);
 		}
 		return user;
 	}
-	
-//	@GetMapping("users/{username}")
-//	public User getByUsername(@PathVariable String username, HttpServletResponse res) {
-//		User user = userSvc.getUserbyUsername(username);
-//		if(user == null) {
-//			res.setStatus(404);
-//		}
-//		return user;
-//	}
-	
+
+	@GetMapping("users/{username}")
+	public User getByUsername(@PathVariable String username, HttpServletResponse res, Principal principal) {
+		User user = userSvc.getUserbyUsername(username);
+		if (user == null) {
+			res.setStatus(404);
+		}
+		return user;
+	}
+
 	@PostMapping("users")
-	public User create(@RequestBody User user, HttpServletResponse res) {
+	public User create(@RequestBody User user, HttpServletResponse res, Principal principal) {
 		User newUser = userSvc.create(user);
-		if(newUser == null) {
+		if (newUser == null) {
 			res.setStatus(400);
 			return null;
 		}
 		res.setStatus(201);
 		return newUser;
 	}
-	
+
 	@PutMapping("users/{userId}")
-	public User update(@RequestBody User user, @PathVariable int userId,HttpServletResponse res) {
+	public User update(@RequestBody User user, @PathVariable int userId, HttpServletResponse res, Principal principal) {
 		return userSvc.update(user, userId);
 	}
-	
+
 	@DeleteMapping("users/{userId}")
-	public void destroy(@PathVariable int userId, HttpServletResponse res) {
-		if(userSvc.deleteUser(userId)) {
+	public void destroy(@PathVariable int userId, HttpServletResponse res, Principal principal) {
+		if (userSvc.deleteUser(userId)) {
 			res.setStatus(204);
-		}else {
+		} else {
 			res.setStatus(404);
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
