010cf25cc108c3eebd0b02145657d56812bc57ab
Steph Karlsen <becomingsteph@gmail.com>
2022-09-26 13:07:43 -0600

    working navbar with toggle

--- ngPerfectDate/src/app/app-routing.module.ts
+++ ngPerfectDate/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { LoginComponent } from './components/login/login.component';
+import { DatenightComponent } from './components/datenight/datenight.component';
+import { CategoryComponent } from './components/category/category.component';
+import { BlogComponent } from './components/blog/blog.component';
 import { HomeComponent } from './components/home/home.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home'},
 { path: 'home', component: HomeComponent },
+{ path: 'blog', component: BlogComponent },
+{ path: 'category', component: CategoryComponent },
+{ path: 'datenight', component: DatenightComponent },
+{ path: 'login', component: LoginComponent},
+{ path: 'user-profile', component: UserProfileComponent },
 { path: '**', component: NotfoundComponent }
 ];
 
---
+++ ngPerfectDate/src/app/components/datenight/datenight.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datenight',
+  templateUrl: './datenight.component.html',
+  styleUrls: ['./datenight.component.css']
+})
+export class DatenightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
