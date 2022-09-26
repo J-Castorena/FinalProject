d7af1997a2e01316b72dfe83170345c5f1284ee9
diegoescutia <escutia.diego@gmail.com>
2022-09-26 19:50:07 -0400

    home update

--- ngPerfectDate/src/app/components/home/home.component.ts
+++ ngPerfectDate/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
+import { AuthService } from 'src/app/services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+loggedInUser: User = new User();
 
-  constructor() { }
+  constructor(private AuthService: AuthService) { }
 
   ngOnInit(): void {
+  this.AuthService.getCredentials();
   }
 
 }
