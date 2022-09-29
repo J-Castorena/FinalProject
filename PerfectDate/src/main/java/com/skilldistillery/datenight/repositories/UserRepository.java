4ce91f93fe34425f111316365b6cca2d453529c1
Jordy <castorena.jordy2@gmail.com>
2022-09-29 10:12:30 -0600

    merge fix

--- PerfectDate/src/main/java/com/skilldistillery/datenight/repositories/UserRepository.java
+++ PerfectDate/src/main/java/com/skilldistillery/datenight/repositories/UserRepository.java
@@ -1,9 +1,5 @@
 package com.skilldistillery.datenight.repositories;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 8aba65c6a17869e20ba3e82db9c31972e6e04ae5
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.datenight.entities.User;
